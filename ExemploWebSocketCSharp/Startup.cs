9d454f09ba7790812704c63a94be7a50d1d8a211
Cleomar Cantil <cleomarcantil@outlook.com>
2021-07-30 02:03:36 -0300

    Conexão básica.

--- ExemploWebSocketCSharp/Startup.cs
+++ ExemploWebSocketCSharp/Startup.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
@@ -10,6 +11,10 @@ using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ExemploWebSocketCSharp
@@ -26,12 +31,13 @@ namespace ExemploWebSocketCSharp
 		// This method gets called by the runtime. Use this method to add services to the container.
 		public void ConfigureServices(IServiceCollection services)
 		{
-
 			services.AddControllers();
 			services.AddSwaggerGen(c =>
 			{
 				c.SwaggerDoc("v1", new OpenApiInfo { Title = "ExemploWebSocketCSharp", Version = "v1" });
 			});
+
+			services.AddRazorPages();
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -50,13 +56,57 @@ namespace ExemploWebSocketCSharp
 
 			app.UseAuthorization();
 
+			app.UseWebSockets(new WebSocketOptions { KeepAliveInterval = TimeSpan.FromSeconds(120) });
+
 			app.UseEndpoints(endpoints =>
 			{
 				endpoints.MapControllers();
-
-				endpoints.MapGet("/", () => $"<h1>{nameof(ExemploWebSocketCSharp)} - Teste</h1>");
+				endpoints.MapRazorPages();
 			});
+
+			app.Use(async (context, next) =>
+			{
+				if (context.Request.Path == "/ws")
+				{
+					if (context.WebSockets.IsWebSocketRequest)
+					{
+						using (WebSocket webSocket = await context.WebSockets.AcceptWebSocketAsync())
+						{
+							await Echo(context, webSocket);
+						}
+					}
+					else
+					{
+						context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+					}
+				}
+				else
+				{
+					await next();
+				}
+
+			});
+
 		}
 
+
+		private async Task Echo(HttpContext context, WebSocket webSocket)
+		{
+			var buffer = new byte[1024 * 4];
+			WebSocketReceiveResult result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+
+			while (!result.CloseStatus.HasValue)
+			{
+				var mensagemRecebida = Encoding.UTF8.GetString(buffer);
+				Console.WriteLine($"Mensagem recebida: {mensagemRecebida}");
+
+				var mensagemParaEnviar = Encoding.UTF8.GetBytes($"Do servidor: {DateTime.Now}");
+				await webSocket.SendAsync(new ArraySegment<byte>(mensagemParaEnviar), result.MessageType, result.EndOfMessage, CancellationToken.None);
+
+				result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+			}
+
+			await webSocket.CloseAsync(result.CloseStatus.Value, result.CloseStatusDescription, CancellationToken.None);
+		}
 	}
 }
