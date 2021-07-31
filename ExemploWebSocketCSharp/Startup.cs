b1f7f138c1475be524b62d96789ca47296a99ecc
Cleomar Cantil <cleomarcantil@outlook.com>
2021-07-30 21:51:56 -0300

    Código do WebSocket movido para uma controller.

--- ExemploWebSocketCSharp/Startup.cs
+++ ExemploWebSocketCSharp/Startup.cs
@@ -64,49 +64,8 @@ namespace ExemploWebSocketCSharp
 				endpoints.MapRazorPages();
 			});
 
-			app.Use(async (context, next) =>
-			{
-				if (context.Request.Path == "/ws")
-				{
-					if (context.WebSockets.IsWebSocketRequest)
-					{
-						using (WebSocket webSocket = await context.WebSockets.AcceptWebSocketAsync())
-						{
-							await Echo(context, webSocket);
-						}
-					}
-					else
-					{
-						context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
-					}
-				}
-				else
-				{
-					await next();
-				}
-
-			});
-
 		}
 
 
-		private async Task Echo(HttpContext context, WebSocket webSocket)
-		{
-			var buffer = new byte[1024 * 4];
-			WebSocketReceiveResult result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
-
-			while (!result.CloseStatus.HasValue)
-			{
-				var mensagemRecebida = Encoding.UTF8.GetString(buffer);
-				Console.WriteLine($"Mensagem recebida: {mensagemRecebida}");
-
-				var mensagemParaEnviar = Encoding.UTF8.GetBytes($"Do servidor: {DateTime.Now}");
-				await webSocket.SendAsync(new ArraySegment<byte>(mensagemParaEnviar), result.MessageType, result.EndOfMessage, CancellationToken.None);
-
-				result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
-			}
-
-			await webSocket.CloseAsync(result.CloseStatus.Value, result.CloseStatusDescription, CancellationToken.None);
-		}
 	}
 }
